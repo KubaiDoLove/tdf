646388f0bdb501fe9356eed0c82a216837a5f338
Kubaidullov <orochimaru@mail.ru>
2020-10-04 01:31:20 +0600

    Fixed bug with importing scss file and added some new templates

--- cmd/component.go
+++ cmd/component.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright © 2020 KubaiDoLove orochimaru@mail.ru
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -108,9 +108,15 @@ var componentCmd = &cobra.Command{
 		}
 		if isScssIncluded {
 			boilerplate = append(boilerplate, fileToWrite{
-				OutputDir: componentDir,
-				Name:      componentName + ".scss",
+				RawTemplate: templates.ComponentStylesTemplate,
+				OutputDir:   componentDir,
+				Name:        componentName + ".scss",
+				TemplateData: componentData{
+					Name: componentName,
+				},
 			})
+			// Pass template with scss
+			boilerplate[0].RawTemplate = templates.ComponentTemplateWithScss
 		}
 
 		if err := writeComponentFiles(boilerplate...); err != nil {
--- templates/component.go
+++ templates/component.go
@@ -5,14 +5,26 @@ const ComponentTemplate = `import React, { FC } from 'react'
 import { {{.Name}}Props } from './types'
 
 const {{.Name}}: FC<{{.Name}}Props> = () => {
-	return (<></>)
+  return <></>
+}
+
+export default {{.Name}}
+`
+
+// ComponentTemplateWithScss represents boilerplate data for React components in TS with Scss file included
+const ComponentTemplateWithScss = `import React, { FC } from 'react'
+import { {{.Name}}Props } from './types'
+import '{{.Name}}.scss'
+
+const {{.Name}}: FC<{{.Name}}Props> = () => {
+  return <div className='{{.Name}}'><div/>
 }
 
 export default {{.Name}}
 `
 
 // ComponentIndexTemplate represents barrel file for React components in TS
-const ComponentIndexTemplate = `export { default } from './{{.Name}}'
+const ComponentIndexTemplate = `export { default as {{.Name}} } from './{{.Name}}'
 `
 
 // ComponentInterfacesTemplate represents interfaces boilerplate data file for React components in TS
@@ -21,3 +33,6 @@ const ComponentInterfacesTemplate = `export interface {{.Name}}Props {}
 
 // ComponentTypesIndexTemplate represents types barrel file for React components in TS
 const ComponentTypesIndexTemplate = `export * from './interfaces'`
+
+// ComponentStylesTemplate represents css/scss file
+const ComponentStylesTemplate = `.{{.Name}} {}`
